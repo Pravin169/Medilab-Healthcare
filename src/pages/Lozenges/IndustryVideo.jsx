98bcf6aa45b9956543bf6aa65c3ae4f2f30fc9ed
pravinshinde09 <138846741+pravinshinde09@users.noreply.github.com>
2024-12-21 14:57:43 +0530

    Add card changes.

--- src/pages/Lozenges/IndustryVideo.jsx
+++ src/pages/Lozenges/IndustryVideo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const IndustryVideo = () => {
   return (
@@ -6,18 +6,22 @@ const IndustryVideo = () => {
       <div style={styles.textContainer}>
         <h2 style={styles.videoTitle}>See How Our Lozenges Are Manufactured</h2>
         <p style={styles.videoDescription}>
-          Watch our industry-leading process in manufacturing lozenges, ensuring the highest quality and safety standards. 
-          This video provides a behind-the-scenes look at our state-of-the-art manufacturing facilities, where every step is 
-          carefully controlled to meet rigorous health and safety guidelines. From ingredient sourcing to packaging, we pride ourselves 
-          on delivering premium products that you can trust.
+          Watch our industry-leading process in manufacturing lozenges, ensuring
+          the highest quality and safety standards. This video provides a
+          behind-the-scenes look at our state-of-the-art manufacturing
+          facilities, where every step is carefully controlled to meet rigorous
+          health and safety guidelines. From ingredient sourcing to packaging,
+          we pride ourselves on delivering premium products that you can trust.
         </p>
         <p style={styles.videoDetails}>
-          Our Lozenges are crafted using the latest technology, which ensures consistency in quality and potency. 
-          The video also highlights how we maintain strict hygiene and quality control measures during production.
+          Our Lozenges are crafted using the latest technology, which ensures
+          consistency in quality and potency. The video also highlights how we
+          maintain strict hygiene and quality control measures during
+          production.
         </p>
       </div>
       <div style={styles.videoWrapper}>
-        <iframe
+        {/* <iframe
           width="100%"
           height="500"
           backgroundColor='white'
@@ -25,7 +29,7 @@ const IndustryVideo = () => {
           title="Industry Manufacturing Video"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
-        ></iframe>
+        ></iframe> */}
       </div>
     </section>
   );
@@ -33,41 +37,41 @@ const IndustryVideo = () => {
 
 const styles = {
   videoSection: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: '40px',
-    paddingBottom: '40px',
-    textAlign: 'center',
-    gap:'30px'
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: "40px",
+    paddingBottom: "40px",
+    textAlign: "center",
+    gap: "30px",
   },
   textContainer: {
-    marginBottom: '20px',
-    maxWidth: '800px',
-    padding:'20px'
+    marginBottom: "20px",
+    maxWidth: "800px",
+    padding: "20px",
   },
   videoTitle: {
-    fontSize: '30px',
-    fontWeight: 'bold',
-    color: '#333',
-    marginBottom: '10px',
+    fontSize: "30px",
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: "10px",
   },
   videoDescription: {
-    fontSize: '18px',
-    color: '#666',
-    lineHeight: '1.6',
-    marginBottom: '20px',
-    backgroundColor:'white'
+    fontSize: "18px",
+    color: "#666",
+    lineHeight: "1.6",
+    marginBottom: "20px",
+    backgroundColor: "white",
   },
   videoDetails: {
-    fontSize: '16px',
-    color: '#555',
-    lineHeight: '1.6',
-    marginTop: '20px',
+    fontSize: "16px",
+    color: "#555",
+    lineHeight: "1.6",
+    marginTop: "20px",
   },
   videoWrapper: {
-    width: '100%',
-    maxWidth: '900px',
+    width: "100%",
+    maxWidth: "900px",
   },
 };
 
--- src/pages/Lozenges/LozengesCard.jsx
+++ src/pages/Lozenges/LozengesCard.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import Cherry from '../../assets/images/Lozenges/cherry.jpeg';
-import Ginger from '../../assets/images/Lozenges/ginger.jpeg';
-import Orange from '../../assets/images/Lozenges/orange.jpeg';
-import Honey from '../../assets/images/Lozenges/honey.jpeg';
-import Mint from '../../assets/images/Lozenges/mint.jpeg';
-import Lemon from '../../assets/images/Lozenges/lemon.jpeg';
+import React from "react";
+import Cherry from "../../assets/images/Lozenges/cherry.jpeg";
+import Ginger from "../../assets/images/Lozenges/ginger.jpeg";
+import Orange from "../../assets/images/Lozenges/orange.jpeg";
+import Honey from "../../assets/images/Lozenges/honey.jpeg";
+import Mint from "../../assets/images/Lozenges/mint.jpeg";
+import Lemon from "../../assets/images/Lozenges/lemon.jpeg";
 
 const LozengesCard = () => {
   const flavors = [
@@ -18,14 +18,23 @@ const LozengesCard = () => {
 
   return (
     <div style={styles.cardContainer}>
-      {flavors.map((flavor, index) => (
-        <div key={index} style={styles.card}>
-          <h3 style={styles.title}>{flavor.title}</h3>
-          <div style={styles.imageBox}>
-            <img src={flavor.img} alt={flavor.title} style={styles.image} />
+      <h3> Flavors </h3>
+      <div style={styles.card}>
+        {flavors.map((flavor, index) => (
+          <div
+            data-aos="zoom-in"
+            key={index}
+            style={{
+              ...styles.cardContent,
+              backgroundImage: `url(${flavor.img})`,
+            }}
+          >
+            <div style={styles.titleContainer}>
+              <h3 style={styles.title}>{flavor.title}</h3>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
@@ -33,38 +42,48 @@ const LozengesCard = () => {
 const styles = {
   cardContainer: {
     display: "flex",
-    justifyContent: "space-around",
+    flexDirection: "column",
     alignItems: "center",
-    flexWrap: "wrap",
+    justifyContent: "center",
     gap: "20px",
     padding: "10px",
+    marginTop: "20px",
   },
   card: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap: "20px",
+  },
+  cardContent: {
     width: "150px",
     height: "200px",
     borderRadius: "10px",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
     display: "flex",
-    justifyContent: "center",
+    flexDirection: "column",
+    justifyContent: "flex-end",
     alignItems: "center",
     boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
     transition: "transform 0.3s",
     cursor: "pointer",
-    flexDirection: "column",
+    position: "relative",
+    overflow: "hidden",
+  },
+  titleContainer: {
+    width: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    padding: "10px",
+    textAlign: "center",
+    borderBottomLeftRadius: "10px",
+    borderBottomRightRadius: "10px",
   },
   title: {
-    fontSize: "18px",
-    textAlign: "center",
+    fontSize: "16px",
+    color: "#fff",
     fontWeight: "bold",
-    marginBottom: "10px",
-  },
-  imageBox: {
-    marginBottom: "10px",  
-  },
-  image: {
-    width: "100px",
-    height: "100px",
-    borderRadius: "50%",
-    objectFit: "cover", 
+    margin: 0,
   },
 };
 
